d9cd88998da6c7ef39e70aa3a886c547562695d2
Ars Paradox <152275094+Iteranya@users.noreply.github.com>
2025-05-22 10:31:29 +0700

    Change host to 127.0.0.1

--- main.py
+++ main.py
@@ -39,4 +39,6 @@ app.include_router(file_route.router)
 # Run the application with: uvicorn main:app --reload
 if __name__ == "__main__":
     
-    uvicorn.run("main:app", host="localhost", port=5451, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=5451, reload=True)
+
+    
